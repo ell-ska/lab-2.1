925c1e05ddbf3419437d498851893dc075886f09
ell-ska <ella@skagerstrom.se>
2023-04-17 09:21:59 +0200

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() {
+
+    return (
+        <header className="header">
+            <h2>hello world</h2>
+        </header>
+    )
+}
+
+export default Header
